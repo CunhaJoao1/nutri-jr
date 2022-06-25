2cd97d71a22ec1dd7fdcd9b589d08e1027942693
João Victor Cunha <joaovictorcunha8@gmail.com>
2022-06-24 22:18:21 -0300

    cards are turning

--- src/Components/Employees/Employees.tsx
+++ src/Components/Employees/Employees.tsx
@@ -43,15 +43,23 @@ export default function Employees() {
                         return( 
                             <div className='w-[min(18.25rem,100%)]  h-80 group perspective cursor-pointer'>
 
-                                <div className='absolute backface-hidden w-full h-full b'>
-                                    <div className='relative'>
-                                        <span className='w-full h-full bg-brand-300/25 absolute inset-0'/>
-                                        <img src={retrato} alt="" />
-                                        <p className='absolute bottom-10 left-2 font-bold text-white text-2xl'>{value.name}</p>
-                                        <p className='absolute bottom-2 left-2 font-normal text-white '>{value.cargo}</p>
+                                <div className='absolute backface-hidden w-full h-full '>
+                                    <div className='relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-500'>
+                                        <div className='absolute backface-hidden  w-full h-full'>
+                                            <span className='w-[min(18.25rem,100%)]  h-[22rem] bg-brand-300/25 absolute inset-0'/>
+                                            <img src={retrato} alt="" />
+                                            <p className='absolute bottom-10 left-2 font-bold text-white text-2xl'>{value.name}</p>
+                                            <p className='absolute bottom-2 left-2 font-normal text-white '>{value.cargo}</p>
+                                        </div>
+                                        <div className='absolute my-rotate-y-180 backface-hidden w-[min(18.25rem,100%)]  h-[22rem] bg-brand-300 overflow-hidden'>
+                                            <div className=' my-10 text-gray-800 px-2 pb-24'>
+                                                <p >{value.description}</p>
+                                            </div>
+                                        </div>
+                                        
                                     </div>
                                 </div>
-                                <p>{value.description}</p>
+                                    <div className="invisible">Lorem ipsum dolor, sit amet consectetur adipisicing elit. At ratione labore harum et, doloremque doloribus fugit! Maiores odit distinctio adipisci! Voluptatum, dicta consequuntur. Dolorem, fuga corporis atque nam quasi sunt.  </div>
 
                             </div>
                         )
